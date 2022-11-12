a47378140649cea3080f30ef72e7111916544d5e
Harish <110305641+harishindl@users.noreply.github.com>
2022-11-12 10:00:48 +0100

    Update jaganannamosam.py

--- jaganannamosam.py
+++ jaganannamosam.py
@@ -454,7 +454,7 @@ st.write(df_trending['Trends'].head(10))
 #st.title("Top Tweets by retweets")
 if df_trending['Trends'].str.contains('JaganannaMosam').any():
     #st.markdown("<h1 style='text-align: center; color: Green;'>Is Trending Now</h1>", unsafe_allow_html=True)
-    rank = df_trending.index[df_trending.Trends == 'JaganannaMosam']
+    rank = df_trending.loc[df_trending.Trends == 'JaganannaMosam']
 
     st.header("Trending now with India wide rank " + int(rank))  
 else:
